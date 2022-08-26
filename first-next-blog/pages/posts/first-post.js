0821fd603399c0771a6cfe2e0a0bd23bfddf0e30
Ilona Zancaner <ilona.zancaner@two-point-o.be>
2022-08-26 16:47:16 +0200

    Start blog from next tutorial

---
+++ first-next-blog/pages/posts/first-post.js
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import Layout from '../../components/layout'
+
+const FirstPost = () => {
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </Layout>
+  )
+}
+
+export default FirstPost
