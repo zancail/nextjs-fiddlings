19c7eb11197858fae7f7cc39c8673cae7a7634dd
Ilona Zancaner <ilona.zancaner@two-point-o.be>
2022-09-08 13:14:41 +0200

    Try out contentful CDA

--- cms-contentful-app/lib/api.js
+++ cms-contentful-app/lib/api.js
@@ -26,104 +26,122 @@ description {
     }
   }
 }
-`
+`;
 
 async function fetchGraphQL(query, preview = false) {
-  return fetch(
-    `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${
-          preview
-            ? process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN
-            : process.env.CONTENTFUL_ACCESS_TOKEN
-        }`,
-      },
-      body: JSON.stringify({ query }),
-    }
-  ).then((response) => response.json())
+    return fetch(
+        `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${
+                    preview
+                        ? process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN
+                        : process.env.CONTENTFUL_ACCESS_TOKEN
+                }`,
+            },
+            body: JSON.stringify({ query }),
+        },
+    ).then((response) => response.json());
 }
 
 function extractPost(fetchResponse) {
-  return fetchResponse?.data?.blogPostCollection?.items?.[0]
+    return fetchResponse?.data?.blogPostCollection?.items?.[0];
 }
 
 function extractPostEntries(fetchResponse) {
-  return fetchResponse?.data?.blogPostCollection?.items
+    return fetchResponse?.data?.blogPostCollection?.items;
 }
 
 export async function getPreviewPostBySlug(slug) {
-  const entry = await fetchGraphQL(
-    `query {
+    const entry = await fetchGraphQL(
+        `query {
       blogPostCollection(where: { slug: "${slug}" }, preview: true, limit: 1) {
         items {
           ${POST_GRAPHQL_FIELDS}
         }
       }
     }`,
-    true
-  )
-  return extractPost(entry)
+        true,
+    );
+    return extractPost(entry);
 }
 
 export async function getAllPostsWithSlug() {
-  const entries = await fetchGraphQL(
-    `query {
+    const entries = await fetchGraphQL(
+        `query {
       blogPostCollection(where: { slug_exists: true }, order: date_DESC) {
         items {
           ${POST_GRAPHQL_FIELDS}
         }
       }
-    }`
-  )
-  return extractPostEntries(entries)
+    }`,
+    );
+    return extractPostEntries(entries);
 }
 
 export async function getAllPostsForHome(preview) {
-  const entries = await fetchGraphQL(
-    `query {
+    const entries = await fetchGraphQL(
+        `query {
       blogPostCollection(limit: 10, order: date_DESC, preview: ${
-        preview ? 'true' : 'false'
+          preview ? "true" : "false"
       }) {
         items {
           ${POST_GRAPHQL_FIELDS}
         }
       }
     }`,
-    preview
-  )
-  return extractPostEntries(entries)
+        preview,
+    );
+    return extractPostEntries(entries);
 }
 
 export async function getPostAndMorePosts(slug, preview) {
-  const entry = await fetchGraphQL(
-    `query {
+    const entry = await fetchGraphQL(
+        `query {
       blogPostCollection(where: { slug: "${slug}" }, preview: ${
-      preview ? 'true' : 'false'
-    }, limit: 1) {
+            preview ? "true" : "false"
+        }, limit: 1) {
         items {
           ${POST_GRAPHQL_FIELDS}
         }
       }
     }`,
-    preview
-  )
-  const entries = await fetchGraphQL(
-    `query {
+        preview,
+    );
+    const entries = await fetchGraphQL(
+        `query {
       blogPostCollection(where: { slug_not_in: "${slug}" }, order: date_DESC, preview: ${
-      preview ? 'true' : 'false'
-    }, limit: 2) {
+            preview ? "true" : "false"
+        }, limit: 2) {
         items {
           ${POST_GRAPHQL_FIELDS}
         }
       }
     }`,
-    preview
-  )
-  return {
-    post: extractPost(entry),
-    morePosts: extractPostEntries(entries),
-  }
+        preview,
+    );
+    return {
+        post: extractPost(entry),
+        morePosts: extractPostEntries(entries),
+    };
 }
+
+var contentful = require("contentful");
+
+var client = contentful.createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+});
+
+export const fetchMenuItems = async () => {
+    // Querying the entries specifying the content type ID
+    let entries = await client.getEntries({
+        content_type: "menuItem",
+    });
+
+    entries = { ...entries.items };
+
+    return entries;
+};
--- cms-contentful-app/pages/index.tsx
+++ cms-contentful-app/pages/index.tsx
@@ -1,50 +1,59 @@
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { getAllPostsForHome } from '../lib/api'
-import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
-import { GetStaticProps } from 'next'
+import Container from "../components/container";
+import MoreStories from "../components/more-stories";
+import HeroPost from "../components/hero-post";
+import Intro from "../components/intro";
+import Layout from "../components/layout";
+import { getAllPostsForHome, fetchMenuItems } from "../lib/api";
+import Head from "next/head";
+import { CMS_NAME } from "../lib/constants";
+import { GetStaticProps } from "next";
 
-const Index = ({ preview, allPosts }) => {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
-  return (
-    <>
-      <Layout preview={preview}>
-        <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
-        </Head>
-        <Container>
-          <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 ? (
-            <MoreStories posts={morePosts} />
-          ) : (
-            <p>No posts</p>
-          )}
-        </Container>
-      </Layout>
-    </>
-  )
-}
+const Index = ({ preview, allPosts, allMenuItems }) => {
+    const heroPost = allPosts[0];
+    const morePosts = allPosts.slice(1);
+    console.log(allMenuItems);
+    return (
+        <>
+            <Layout preview={preview}>
+                <Head>
+                    <title>Next.js Blog Example with {CMS_NAME}</title>
+                </Head>
+                <Container>
+                    <Intro />
+                    {heroPost && (
+                        <HeroPost
+                            title={heroPost.title}
+                            coverImage={heroPost.coverImage}
+                            date={heroPost.date}
+                            author={heroPost.author}
+                            slug={heroPost.slug}
+                            excerpt={heroPost.excerpt}
+                        />
+                    )}
+                    {morePosts.length > 0 ? (
+                        <MoreStories posts={morePosts} />
+                    ) : (
+                        <p>No posts</p>
+                    )}
+                    {/* {allMenuItems && (
+                        <ul>
+                            {allMenuItems.items.map((menuItem) => (
+                                <li key={menuItem.sys.id}>{menuItem.title}</li>
+                            ))}
+                        </ul>
+                    )} */}
+                </Container>
+            </Layout>
+        </>
+    );
+};
 
 export const getStaticProps: GetStaticProps = async ({ preview = false }) => {
-  const allPosts = (await getAllPostsForHome(preview)) ?? []
-  return {
-    props: { preview, allPosts },
-  }
-}
+    const allPosts = (await getAllPostsForHome(preview)) ?? [];
+    const allMenuItems = (await fetchMenuItems()) ?? [];
+    return {
+        props: { preview, allPosts, allMenuItems },
+    };
+};
 
-export default Index
+export default Index;
